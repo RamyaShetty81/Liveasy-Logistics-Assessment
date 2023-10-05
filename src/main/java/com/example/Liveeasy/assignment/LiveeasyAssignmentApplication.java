e756376cc3656ee26b16f516b2a238939f85c831
RamyaShetty81 <ramyarshetty81@gmail.com>
2023-10-05 16:20:38 +0530

    commit

---
+++ src/main/java/com/example/Liveeasy/assignment/LiveeasyAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Liveeasy.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveeasyAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveeasyAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Liveeasy/assignment/LiveeasyAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Liveeasy.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveeasyAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
