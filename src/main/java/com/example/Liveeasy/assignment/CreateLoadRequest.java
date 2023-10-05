7ea20b185242cb4f16ef19d9329a27264933aaf0
RamyaShetty81 <120573688+RamyaShetty81@users.noreply.github.com>
2023-10-05 16:37:45 +0530

    Create CreateLoadRequest.java

---
+++ src/main/java/com/example/Liveeasy/assignment/CreateLoadRequest.java
@@ -0,0 +1,25 @@
+package com.example.loads.Apis.Load.DTOs;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class CreateLoadRequest  {
+    private String loadingPoint;
+    private String unloadingPoint;
+    private String productType;
+    private String truckType;
+    private String noOfTrucks;
+    private String weight;
+    private String optional;
+    private String shipperId;
+    private String date;  
+}
