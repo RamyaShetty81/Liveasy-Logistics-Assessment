3a93604fb51515f3c78ea16a78c087a16368d032
RamyaShetty81 <120573688+RamyaShetty81@users.noreply.github.com>
2023-10-05 16:33:20 +0530

    Create LoadService.java

---
+++ src/main/java/com/example/Liveeasy/assignment/LoadService.java
@@ -0,0 +1,17 @@
+package com.example.Liveasy.assignment;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+import com.example.loads.Apis.Load.DTOs.CreateLoadRequest;
+import com.example.loads.Apis.Load.DTOs.UpdateLoadRequest;
+
+@Service
+public interface LoadService {
+    public String addLoad(CreateLoadRequest createLoadRequest);
+    public List<Load> getLoadsByShipperId(String shipperId) throws Exception;
+    public Load getLoadByLoadId(Integer loadId) throws Exception;
+    public String updateLoadById(Integer loadId, UpdateLoadRequest UpdateLoadRequest) throws Exception;
+    public String deleteLoadById(Integer loadId) throws Exception;
+
+}
